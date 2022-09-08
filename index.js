30a1d17f39cea7965743e605f0e08904998628a3
korsakovi <mat2@hotmail.es>
2022-09-08 18:38:11 +0200

    components package published npm

---
+++ index.js
@@ -0,0 +1,12 @@
+import Button from './atoms/Button';
+import Heading from './atoms/Heading';
+import Icon from './atoms/Icon';
+import Input from './atoms/Input';
+import Picture from './atoms/Picture';
+
+import Spacer from './layout/Spacer';
+
+import ButtonIcon from './molecules/ButtonIcon';
+import DropDown from './molecules/Dropdown';
+
+export { Button, Heading, Icon, Input, Picture, Spacer, ButtonIcon, DropDown };
